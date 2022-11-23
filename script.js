b805a5ab89e7043543dc5bbd20d27cdebf698793
Ramin-Hashemi <85808176+Ramin-Hashemi@users.noreply.github.com>
2022-11-23 16:06:39 +0330

    Adding Starting Learning Project

---
+++ script.js
@@ -0,0 +1,150 @@
+// javaScript Fundamentals
+
+
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+
+// ???
+
+// var firstName = "Ramin";
+// var lastName = "Hashemi";
+// var fullName = firstName + " " + lastName;
+// alert(fullName);
+
+
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+
+// ???
+
+// var firstEntry = prompt("What is your first name?");
+// var secondEntry = prompt("What is your last name?");
+// var fullEntry = firstEntry + " " + secondEntry;
+// alert("Your full name is" + " " + fullEntry);
+// alert("Congratulations!");
+
+
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+
+// ???
+
+// var firstNum = prompt("What is you age?");
+// var secondNum = prompt("What is the age of you retierment?");
+// alert("You have to work for next " + (secondNum - firstNum) + " years!");
+
+
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+
+// ???
+
+// var firstName = "Ramin";
+// var lastName = "Hashemi";
+// var fullName = firstName + " " + lastName;
+// var firstEntry = prompt("What is your first name?");
+// var secondEntry = prompt("What is your last name?");
+// var fullEntry = firstEntry + " " + secondEntry;
+// if (firstEntry === firstName && secondEntry === lastName) {
+// 	alert("Congratulations! " + "You have solved the problem");
+// } else if (firstEntry !== firstName && secondEntry !== lastName) {
+// 	alert ("Your Last Name and Last Name are not matched!");
+// }
+// if (firstEntry !== firstName && secondEntry === lastName) {
+// 	alert("Sorry, Your First Name is not matched!");
+// } else if (firstEntry === firstName && secondEntry !== lastName) {
+// 	alert("Sorry, Your Last Name is not matched!");
+// }
+// if (firstEntry === firstName && secondEntry === lastName) {
+// 	alert("Your full name is" + " " + fullEntry);
+// }
+
+
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+
+// Function Declaration
+
+// function sayHello() {
+// 	alert("Hello Ramin!");
+// 	console.log("Hello Ramin!");
+// }
+// sayHello();
+
+
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+
+// Function Expression
+
+// var sayBye = function goodBye() {
+// 	alert("Goodbye Ramin!");
+// 	console.log("Goodbye Ramin!");
+// }
+// sayBye();
+
+
+// Anonymous Function
+
+// var sayBye = function () {
+// 	alert("Bye Ramin!");
+// 	console.log("Bye Ramin!");
+// }
+// sayBye();
+
+
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+
+// Extensivability
+
+// function sing(song) {
+// 	console.log(song);
+// }
+// sing("Laaa Laaa Laaa");
+// sing("LaLa LaLa La");
+// sing("LaLaLi LiLaLaiLai");
+
+
+// Or
+
+// function multiply(a, b) {
+// 	return a * b;
+// }
+// alert(multiply(3, 6));
+
+
+// Or
+
+var math = function (a, b) {
+	return a * b;
+}
+alert(math(3, 6));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
